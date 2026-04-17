6216c4e3a8b561c4362badad64c789daed1e7266
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-04-17 20:18:58 +0200

    añado logica para a partir introducir 2 puntos te calcule un tercero

--- lib/models/fila_patron_equipo.dart
+++ lib/models/fila_patron_equipo.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class FilaPatronEquipo {
   final TextEditingController patron;
   final TextEditingController equipo;
+  bool esManual = false;
+  bool estaCalculado = false;
 
   FilaPatronEquipo({required this.patron, required this.equipo});
 }
--- lib/viewModels/home_view_model.dart
+++ lib/viewModels/home_view_model.dart
@@ -2,6 +2,8 @@ import 'package:extra_pol/models/fila_patron_equipo.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewModel extends ChangeNotifier {
+  bool _isupdating = false;
+
   final List<FilaPatronEquipo> puntos = [
     FilaPatronEquipo(
       patron: TextEditingController(),
@@ -25,29 +27,66 @@ class HomeViewModel extends ChangeNotifier {
   }
 
   void _onChanged() {
+    //Si esta calculando no hago nada mas
+    if (_isupdating) {
+      return;
+    }
+    //Creamos un bool por fila para compraobar si esta la pareja vacia o no con el fin de bloquear o no.
+    bool f1 =
+        puntos[0].patron.text.isNotEmpty && puntos[0].equipo.text.isNotEmpty;
+    bool f2 =
+        puntos[1].patron.text.isNotEmpty && puntos[1].equipo.text.isNotEmpty;
+    bool f3 =
+        puntos[2].patron.text.isNotEmpty && puntos[2].equipo.text.isNotEmpty;
+
+    //Recorremos filas y si se cumple la condicion la marcamos como manual
+    for (var punto in puntos) {
+      if ((punto.patron.text.isNotEmpty || punto.equipo.text.isNotEmpty) &&
+          !punto.estaCalculado) {
+        punto.esManual = true;
+      }
+    }
+
     //creamos variables nulables y la intentamos pasar a dobles(tryParse)
+
     double? p1 = double.tryParse(puntos[0].patron.text);
     double? e1 = double.tryParse(puntos[0].equipo.text);
-    //Si no son nulas creamos igual den el punto 2
-    if (p1 != null && e1 != null) {
-      double? p2 = double.tryParse(puntos[1].patron.text);
-      double? e2 = double.tryParse(puntos[1].equipo.text);
+    double? p2 = double.tryParse(puntos[1].patron.text);
+    double? e2 = double.tryParse(puntos[1].equipo.text);
+    double? p3 = double.tryParse(puntos[2].patron.text);
+    double? e3 = double.tryParse(puntos[2].equipo.text);
+    //caso 1 (fila 1 y fila 2)
+    if (f1 && f2 && !puntos[2].esManual) {
+      _isupdating = true;
+      double diffP = p2! - p1!;
+      double diffE = e2! - e1!;
+      double p3Res = p2! + diffP;
+      double e3Res = e2! + diffE;
+      puntos[2].patron.text = p3Res.toStringAsFixed(2);
+      puntos[2].equipo.text = e3Res.toStringAsFixed(2);
+      puntos[2].estaCalculado = true;
+      _isupdating = false;
+      //Caso 2 (fila 2 y fila 3)
+    } else if (f2 && f3 && !puntos[0].esManual) {
+      _isupdating = true;
+      double diffP = p3! - p2!;
+      double diffE = e3! - e2!;
+      double p1Res = p2! - diffP;
+      double e1Res = e2! - diffE;
+      puntos[0].patron.text = p1Res.toStringAsFixed(2);
+      puntos[0].equipo.text = e1Res.toStringAsFixed(2);
+      puntos[0].estaCalculado = true;
+      _isupdating = false;
+      //Caso (fila 1 y fila 3)
+    } else if (f1 && f3 && !puntos[1].esManual) {
+      _isupdating = true;
 
-      //comprobamos si tenemos ya dos filas completas
-      if (p2 != null && e2 != null) {
-        double diffP = p2 - p1;
-        double diffE = e2 - e1;
-        double p3Calculado = p2 + diffP;
-        double e3Calculado = e2 + diffE;
-
-        if (puntos[2].patron.text != p3Calculado.toStringAsFixed(2)) {
-          puntos[2].patron.text = p3Calculado.toStringAsFixed(2);
-        }
-
-        if (puntos[2].equipo.text != e3Calculado.toStringAsFixed(2)) {
-          puntos[2].equipo.text = e3Calculado.toStringAsFixed(2);
-        }
-      }
+      double p2Res = (p1! + p3!) / 2;
+      double e2Res = (e1! + e3!) / 2;
+      puntos[1].patron.text = p2Res.toStringAsFixed(2);
+      puntos[1].equipo.text = e2Res.toStringAsFixed(2);
+      puntos[1].estaCalculado = true;
+      _isupdating = false;
     }
   }
 
