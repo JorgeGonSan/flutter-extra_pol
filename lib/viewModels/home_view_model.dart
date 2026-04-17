d83b5e2a208be652bed16c560862e602978b5eec
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2026-04-17 19:20:43 +0200

    ralizo prueba de rellenar p1 y p2 calcule el p3

--- lib/viewModels/home_view_model.dart
+++ lib/viewModels/home_view_model.dart
@@ -16,6 +16,40 @@ class HomeViewModel extends ChangeNotifier {
       equipo: TextEditingController(),
     ),
   ];
+  //Contructor que crea un "Listener" para cada TexEditingController
+  HomeViewModel() {
+    for (var punto in puntos) {
+      punto.patron.addListener(_onChanged);
+      punto.equipo.addListener(_onChanged);
+    }
+  }
+
+  void _onChanged() {
+    //creamos variables nulables y la intentamos pasar a dobles(tryParse)
+    double? p1 = double.tryParse(puntos[0].patron.text);
+    double? e1 = double.tryParse(puntos[0].equipo.text);
+    //Si no son nulas creamos igual den el punto 2
+    if (p1 != null && e1 != null) {
+      double? p2 = double.tryParse(puntos[1].patron.text);
+      double? e2 = double.tryParse(puntos[1].equipo.text);
+
+      //comprobamos si tenemos ya dos filas completas
+      if (p2 != null && e2 != null) {
+        double diffP = p2 - p1;
+        double diffE = e2 - e1;
+        double p3Calculado = p2 + diffP;
+        double e3Calculado = e2 + diffE;
+
+        if (puntos[2].patron.text != p3Calculado.toStringAsFixed(2)) {
+          puntos[2].patron.text = p3Calculado.toStringAsFixed(2);
+        }
+
+        if (puntos[2].equipo.text != e3Calculado.toStringAsFixed(2)) {
+          puntos[2].equipo.text = e3Calculado.toStringAsFixed(2);
+        }
+      }
+    }
+  }
 
   @override
   void dispose() {
